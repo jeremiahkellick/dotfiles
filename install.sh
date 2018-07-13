76fd75913a737b6032b67d9cadd3369715ddbf27
ThaBullfrog <th3bullfrog@gmail.com>
2018-07-13 13:55:21 -0400

    add git to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 source install/brew.sh
+source install/git.sh
 source install/rbenv.sh
 source install/symlink.sh
---
+++ install/git.sh
@@ -0,0 +1,7 @@
+if [ ! -x "$(command -v git)" ]; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        sudo apt-get install git
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install git
+    fi
+fi
