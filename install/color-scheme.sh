0f9c3658de4b2c4ab134f38e9c30feb4013fcf3f
ThaBullfrog <th3bullfrog@gmail.com>
2018-07-13 13:53:05 -0400

    fix install/color-scheme.sh

--- install/color-scheme.sh
+++ install/color-scheme.sh
@@ -3,4 +3,4 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install -y dconf-cli
 fi
-wget -O xt  https://git.io/v5m1l && chmod +x xt && ./xt && rm xt
+wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
