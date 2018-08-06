0be3e0c8ccfd7a346d4b96fc7a00ff2d4b6d4fcc
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-08-06 01:53:48 -0400

    restrict terminfos to macOS

--- install/terminfos.sh
+++ install/terminfos.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-terminfos=$(ls ~/.terminfo/*/*)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    terminfos=$(ls ~/.terminfo/*/*)
 
-if [[ ! $terminfos = *"italic"* ]]; then
-    path1=~/dotfiles/install/terminfos/xterm-256color-italic.terminfo
-    path2=~/dotfiles/install/terminfos/tmux-256color.terminfo
-    tic -x $path1
-    tic -x $path2
+    if [[ ! $terminfos = *"italic"* ]]; then
+        path1=~/dotfiles/install/terminfos/xterm-256color-italic.terminfo
+        path2=~/dotfiles/install/terminfos/tmux-256color.terminfo
+        tic -x $path1
+        tic -x $path2
+    fi
 fi
-
