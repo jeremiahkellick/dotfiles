14c0c041843875613844e2ba9f620137be47e3b5
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2019-01-30 18:18:23 -0500

    Remove support for Ubuntu 16.04

--- install/vim.sh
+++ install/vim.sh
@@ -7,9 +7,3 @@ if [ ! -x "$(command -v vim)" ]; then
         brew install vim
     fi
 fi
-
-if [[ $(vim --version | grep VIM) = *"7.4"* ]]; then
-    sudo add-apt-repository -y ppa:jonathonf/vim
-    sudo apt-get update
-    sudo apt-get -y install vim
-fi
