d552fb0b817f063ee7fad8a0ab5587b9af6e21c9
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2019-01-30 18:59:10 -0500

    Remove Git from install script

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ source install/symlink.sh
 source install/brew.sh
 source install/update.sh
 source install/curl.sh
-source install/git.sh
 source install/tmux.sh
 source install/vim.sh
 source install/rbenv.sh
--- install/git.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -x "$(command -v git)" ]; then
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        sudo apt-get -y install git
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        brew install git
-    fi
-fi
