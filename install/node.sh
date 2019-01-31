63e4390d532ebb28743e497774e9d72699045fab
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2019-01-30 20:49:55 -0500

    Fix Node.js install script

--- install/node.sh
+++ install/node.sh
@@ -1,5 +1,14 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh \
-    | bash
-nvm install --lts
-nvm use --lts
-nvm alias default 'lts/*'
+#!/usr/bin/env bash
+
+if [ ! -x "$(command -v nvm)" ]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh \
+        | bash
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+    nvm install --lts
+    nvm use --lts
+    nvm alias default 'lts/*'
+fi
