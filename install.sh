f248ff75a85e973554f7b238791a83f1f3ddbfe8
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2023-08-24 21:42:08 -0400

    Switch to a default of 4 spaces per tab

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ install_vscode=false
 read -p "Would you like to install node [y/n]? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  install_node=true
+    install_node=true
 fi
 
 read -p "Would you like to install vscode [y/n]? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  install_vscode=true
+    install_vscode=true
 fi
 
 source install/symlink.sh
@@ -23,11 +23,11 @@ source install/tmux.sh
 source install/vim.sh
 
 if [ "$install_node" = true ]; then
-  source install/node.sh
+    source install/node.sh
 fi
 
 if [ "$install_vscode" = true ]; then
-  source install/vscode.sh
+    source install/vscode.sh
 fi
 
 source install/terminfos.sh
